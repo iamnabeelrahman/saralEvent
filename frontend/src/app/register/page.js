0a702e75b33820c9eab468853681fd43d2d049ca
Nabeel Rahman <122614622+iamnabeelrahman@users.noreply.github.com>
2024-11-27 11:43:46 +0530

    Update page.js

--- frontend/src/app/register/page.js
+++ frontend/src/app/register/page.js
@@ -1,7 +1,7 @@
 // src/app/register/page.js
 "use client";
 import RegisterForm from "../components/RegisterForm";
-import styles from "../../styles/Register.module.css";
+import styles from "../styles/Register.module.css";
 
 export default function RegisterPage() {
   return (
