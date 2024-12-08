684d0014e6a117e061b9482b79e6970ca81a5ec9
Nabeel Rahman <122614622+iamnabeelrahman@users.noreply.github.com>
2024-12-08 23:08:16 +0530

    Update middlewares.js

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -15,11 +15,20 @@ module.exports = [
       },
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // Allow requests from frontend
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+      headers: ['Content-Type', 'Authorization'], // Allow Authorization header
+      credentials: true, // Allow sending credentials (cookies, authorization headers)
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+
 ];
