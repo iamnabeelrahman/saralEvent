4b825f324767223101fb5e83cd2e985f4a5c7733
Nabeel Rahman <122614622+iamnabeelrahman@users.noreply.github.com>
2024-12-11 01:55:34 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images:{
-        domains:['localhost', 't4.ftcdn.net']
+        domains:['localhost', 't4.ftcdn.net', 'saralevent-backend.onrender.com']
     }
 };
 
